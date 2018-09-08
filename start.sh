5c7ab30cbae8071eab7ac581febda9bb96c51f2a
chandrakant kaski <chandk@thoughtworks.com>
2018-09-08 23:58:16 +0530

    Making game available for everyone

---
+++ start.sh
@@ -0,0 +1,5 @@
+npm install charm --save
+npm install event --save
+npm install readline --save
+curl https://raw.githubusercontent.com/kaskichandrakant/snakeNodeBased/master/snake.js >gameData.js
+node snake.js
