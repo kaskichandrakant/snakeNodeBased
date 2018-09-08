cfbf6cb37f7c7da69bf12c2b122877f4fa906306
chandrakant kaski <chandk@thoughtworks.com>
2018-09-09 00:04:30 +0530

    Fixing issue

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 npm install charm --save
 npm install event --save
 npm install readline --save
-curl https://raw.githubusercontent.com/kaskichandrakant/snakeNodeBased/master/snake.js >gameData.js
+curl https://raw.githubusercontent.com/kaskichandrakant/snakeNodeBased/master/snake.js >snake.js
 node snake.js
